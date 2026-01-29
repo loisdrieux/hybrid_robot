f11ead5ce30769421475ad72ecb8b4315e61f602
lois <lois@Ubuntu.myguest.virtualbox.org>
2026-01-29 18:29:50 +0100

    changes

--- hybrid_robot_ws/src/hybrid_robot_description/scripts/rrt_planner_node.py
+++ hybrid_robot_ws/src/hybrid_robot_description/scripts/rrt_planner_node.py
@@ -39,7 +39,7 @@ class RRTPlannerNode(Node):
         )
 
         # Parameters
-        self.step_size = 0.1  # Planning step in meters
+        self.step_size = 0.5  # Planning step in meters
         self.control_timer = None 
         self.map_data = None
         self.latest_goal = None
